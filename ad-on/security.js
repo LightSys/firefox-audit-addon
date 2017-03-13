c69a6c4ce075efb679ac2f93dcacc46d2c1a7586
Taylor <austintaylordavy@letu.edu>
2017-03-13 17:02:51 -0500

    Can read in json config file.

--- ad-on/security.js
+++ ad-on/security.js
@@ -1,1 +1,9 @@
-window.alert("No content added at this time")
+console.log("start here");
+$.ajax({
+  url: "http://10.5.128.71/sample-config.json",
+  dataType: "json",
+  success: function(response) {
+    console.log(response);
+    console.log("success");
+  }
+});
