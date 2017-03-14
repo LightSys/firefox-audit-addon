3b4e068cff8da8c5b7d46ac84ef6105f1e104f16
nozzrel <tech7301@gmail.com>
2017-03-14 15:04:36 -0500

    Updated the enumeration functions and wrote a test for the functions.

--- x-audit/enumeration.js
+++ x-audit/enumeration.js
@@ -1,12 +1,15 @@
 //Enumeration functinos to get the list of all add-ons installed and get the values of a set of prefrences
+exports.getAddonList = getAddonList;
+
 
 //Gets a associated array of the ids and names of all add-ons installed 
 function getAddonList() {
 	const { AddonManager } = require("resource://gre/modules/AddonManager.jsm");
 	var addons = [];
-	AddonManager.getAllAddons(function(aAddons){aAddons.foreach(function(aAddon, index){
+	var aAddons = [];
+	AddonManager.getAllAddons(function(aAddons){aAddons.forEach(function(aAddon, i){
 		var addonObject = {id: aAddon.id, name: aAddon.name};
-		addons[index] = addonObject;  
+		addons[i] = addonObject;
 	});});
 	return addons;
 }
@@ -15,7 +18,7 @@ function getAddonList() {
 function getPrefList(settings) {
 	var prefs = Component.classes["@mozilla.org/preferences-services;1"].getService(Components.interface.nsIPrefBranch);
 	var values = [];
-	settings.foreach(function(setting,index){
+	settings.forEach(function(setting,index){
 		var type = prefs.getPrefType(setting);
 		if(type == "PREF_BOOL") values[i] = prefs.getPrefBool(setting);
 		else if (type == "PREF_STRING") values[i] = prefs.getPrefChar(setting);
--- x-audit/index.js
+++ x-audit/index.js
@@ -1,14 +1,16 @@
 var self = require("sdk/self");
+var enumeration = require("enumeration");
 
 // a dummy function, to show how tests work.
 // to see how to test this function, look at test/test-index.js
-function dummy(text, callback) {
-	alert("test");
-	var addons = getAddonList();
+function dummy() {
+//	alert("test");
+	var addons = enumeration.getAddonList();
 	var settings = ["accessibility.tabfocus", "advanced.mailftp", "dave"];
-	var prefs = getPrefList(settings);
-	addons.foreach(function(addon){console.log(addon.name);});
-	prefs.foreach(function(value){console.log(value);});
+	//var prefs = enumeration.getPrefList(settings);
+	//addons.forEach(function(addon){console.log(addon.name);});
+	//prefs.forEach(function(value){console.log(value);});
+	console.log(addons);
 }
 
 exports.main = dummy;
