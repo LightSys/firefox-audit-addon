f6b74e5ae3b4944ad3cd5257dae661a89b850259
Taylor <austintaylordavy@letu.edu>
2017-03-14 15:49:55 -0500

    x-audit enumeration cleaned up.

--- x-audit/enumeration.js
+++ x-audit/enumeration.js
@@ -1,30 +1,27 @@
-//Enumeration functinos to get the list of all add-ons installed and get the values of a set of prefrences
-exports.getAddonList = getAddonList;
+//Gets an associated array of the ids and names of all add-ons installed 
 
-
-//Gets a associated array of the ids and names of all add-ons installed 
 function getAddonList() {
 	const { AddonManager } = require("resource://gre/modules/AddonManager.jsm");
+	var addonId = "";
+	var addonName = "";
+	var addon = {};
 	var addons = [];
-	var aAddons = [];
-	AddonManager.getAllAddons(function(aAddons){aAddons.forEach(function(aAddon, i){
-		var addonObject = {id: aAddon.id, name: aAddon.name};
-		addons[i] = addonObject;
-	});});
+	var allAddons = [];
+	var addonObject = {};
+	AddonManager.getAllAddons(function(allAddons){
+		for (i = 0; i < allAddons.length; i++){
+		addon = allAddons[i]; 
+		addonId = (addon.id).toString();	
+		addonName = (addon.name).toString(); 
+		addonObject = {id: addonId, name: addonName};
+		addons.push(addonObject);
+		}
 	return addons;
+
+	});
+
+	return -1;
 }
 
-//Gets an array of the value of all settings requested
-function getPrefList(settings) {
-	var prefs = Component.classes["@mozilla.org/preferences-services;1"].getService(Components.interface.nsIPrefBranch);
-	var values = [];
-	settings.forEach(function(setting,index){
-		var type = prefs.getPrefType(setting);
-		if(type == "PREF_BOOL") values[i] = prefs.getPrefBool(setting);
-		else if (type == "PREF_STRING") values[i] = prefs.getPrefChar(setting);
-		else if (type == "PREF_INT") values[i] = pref.getPrefInt(setting);
-		else if (type == "PREF_INVALID") values[i] = "error";
-	});
-	return values;
-}
+exports.getAddonList = getAddonList;
 
--- x-audit/index.js
+++ x-audit/index.js
@@ -1,16 +1,11 @@
 var self = require("sdk/self");
 var enumeration = require("enumeration");
+var settings = require("settings");
 
-// a dummy function, to show how tests work.
-// to see how to test this function, look at test/test-index.js
-function dummy() {
-//	alert("test");
-	var addons = enumeration.getAddonList();
-	var settings = ["accessibility.tabfocus", "advanced.mailftp", "dave"];
-	//var prefs = enumeration.getPrefList(settings);
-	//addons.forEach(function(addon){console.log(addon.name);});
-	//prefs.forEach(function(value){console.log(value);});
-	console.log(addons);
+function init() {
+	enumeration.getAddonList();
+	//settings.getPrefList(settings);
+	
 }
 
-exports.main = dummy;
+exports.main = init;
