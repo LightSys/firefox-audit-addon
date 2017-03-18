f2fa911a6d6d17b296f5de6beabc3a0834360fe9
Taylor <austintaylordavy@letu.edu>
2017-03-17 21:08:10 -0500

    Web Crypto API partially implemented.

--- sdk-audit/lib/hashing.js
+++ sdk-audit/lib/hashing.js
@@ -1,14 +1,27 @@
-var CryptoJS = require("cryptojs").Crypto;
-export.dataGen = dataGen;
+exports.dataGen = dataGen;
+
+let { Cc, Ci, Cu } = require('chrome');
+let { TextEncoder, TextDecoder } = require('sdk/io/buffer')
+Cu.importGlobalProperties(['crypto']);
+var utils = require('sdk/window/utils');
+
 
 function dataGen(config, message) { 
-	var key = config;
-	console.log(key);
-	key = hashText256(key);
-	console.log(key);
-	var  salt = saltGen();
-	var data = salt.concat(message);
-	return data;
+	var data = Promise.resolve(saltGen()).then(result => {
+		var salt = result;
+		console.log("salt");
+		console.log(salt);
+		var key = Promise
+			.resolve(win.crypto.subtle.
+		var message = salt + message;
+		var buffer = new TextEncoder("utf-8").encode(data);
+		message = Promise
+			.resolve(win.crypto.subtle.sign({name: "HMAC", hash: "SHA-256"}, key, buffer)
+			.then(signature => {return signature;}));
+		console.log(message);
+		return hex(message);
+	});
+	console.log(data);
 }
 
 
@@ -16,19 +29,20 @@ function dataGen(config, message) {
 function saltGen(){
 	var browserValues = new Uint32Array(512);
 	var browserValue = new Uint32Array(1);
-	hashText512(window.crypto.getRandomValues(browserValues));
-	hashText512(window.crypto.getRandomValues(browserValue));
+	var win = utils.getMostRecentBrowserWindow();
+	hashText512(win.crypto.getRandomValues(browserValues));
 	var seed = seedGen(browserValues);
-	var state = hashText3(seed);
+	var state = hashText512(seed);
 	var whichHash;
-	for(var i = 0; i<3; i++){
-		whichHash = randThree();
+	for(var i = 0; i < 10; i++){
+		hashText512(win.crypto.getRandomValues(browserValue));
+		whichHash = randThree(browserValue);
 		switch(whichHash){
 			case 0:
-				state = hashText5(state);
+				state = hashText256(state);
 				break;
 			case 1:
-				state = hashText3(state);
+				state = hashText384(state);
 				break;
 			case 2:
 				state = hashText512(state);
@@ -36,40 +50,53 @@ function saltGen(){
 		}
 	
 	}
-	var salt = hashText5(state);
+	var salt = hashText256(state);
 	return salt;
 }
 
 
-//Converts Hex to Bianary.
-function Hex2Bin(n){return parseInt(n,16).toString(2);} 
-
-
-//encrypts the text given to it in Sha3 and returns it.
-function hashText3(str){
-	var hash = CryptoJS.SHA3(str);
-	return hash.toString(CryptoJS.enc.Hex);
+//Converts Buffer to Hex.
+function hex(buffer) {
+  var hexCodes = [];
+  var view = new DataView(buffer);
+  for (var i = 0; i < view.byteLength; i += 4) {
+    // Using getUint32 reduces the number of iterations needed (we process 4 bytes each time)
+    var value = view.getUint32(i);
+    // toString(16) will give the hex representation of the number without padding
+    var stringValue = value.toString(16);
+    // We use concatenation and slice for padding
+    var padding = '00000000';
+    var paddedValue = (padding + stringValue).slice(-padding.length);
+    hexCodes.push(paddedValue);
+  }
+  // Join all the hex strings into one
+  return hexCodes.join("");
 }
-
 
 
 //encrypts the text given to it in Sha256 and returns it.
 function hashText256(str){
-	var hash =  CryptoJS.SHA256(str);
-	return hash.toString(CryptoJS.enc.Hex);
+  var buffer = new TextEncoder("utf-8").encode(str);
+  return crypto.subtle.digest("SHA-256", buffer).then(function (hash) {
+    return hex(hash);
+  });
 }
 
+
+//encryptss the text given to it in Sha384 and returns it.
+function hashText384(str){
+  var buffer = new TextEncoder("utf-8").encode(str);
+  return crypto.subtle.digest("SHA-384", buffer).then(function (hash) {
+    return hex(hash);
+  });
+}
 
 //encrypts the text given to it in Sha512 and returns it.
 function hashText512(str){
-	var hash =  CryptoJS.SHA512(str);
-	return hash.toString(CryptoJS.enc.Hex);
-}
-
-//encrypts the text given to it in MD5 and returns it.
-function hashText5(str){
-	var hash =  CryptoJS.MD5(str);	
-	return hash.toString(CryptoJS.enc.Hex);
+  var buffer = new TextEncoder("utf-8").encode(str);
+  return crypto.subtle.digest("SHA-512", buffer).then(function (hash) {
+    return hex(hash);
+  });
 }
 
 //Generates a random number 0,1, or 2.
@@ -90,7 +117,7 @@ function seedGen(randomValues){
 	var numString = "";
 	for(var i=0; i<randomValues.length; i++){
 		numString = (randomValues[i].toString()).charAt(randTen());
-		randString = randString.concat(numString);
+		randString += numString;
 	}
 	return randString;
 }
