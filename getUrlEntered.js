334a677cdb0e96cef4fd317d553226df02b147e5
SirRichard <37304708+SirRichard@users.noreply.github.com>
2018-03-13 16:55:02 -0600

    New Comments

--- getUrlEntered.js
+++ getUrlEntered.js
@@ -1,4 +1,8 @@
 /*
+*	What if we converted this to be "checkURL.js" where we recieve and 
+*	compare the URL hash to the urlList in testConfig, sending the end
+*	result to changeHeader.js?
+*	
 *	getUrlEntered.js retrieves user input from the url bar and hashes
 *	the URL to compare with the hashes stored in the configuration file.
 */
@@ -7,6 +11,8 @@ var doneGot;
 
 browser.webNavigation.onBeforeNavigate.addListener(listened => {
 	doneGot = listened.url;
+	
+	
 	
 	//
 });
