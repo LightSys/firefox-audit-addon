c3ba4cc25adf43d9b804c621d32f9a4eb0ca89e9
John Michael Guynn <mguynn08@gmail.com>
2018-03-15 15:05:26 -0600

    New JS File that contains the code to get the Firefox Version

---
+++ getVersion.js
@@ -0,0 +1,9 @@
+var getting = distribution.iniFile.exists.appversion.get({});
+
+getting.then((got) => {
+  
+  console.log('Value: ${got.value}');
+  console.log('Control: ${got.levelOfControl}');
+  
+});
+
